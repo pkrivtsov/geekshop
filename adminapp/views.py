7221c857f92e47867ee63ec780bc06173e7c2ccd
pkrivtsov <krivtsov@live.com>
2021-02-12 17:52:50 +0300

    own adminapp: CRUD + @user_passes_test

---
+++ adminapp/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
+
+from authapp.models import User
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
+
+
+@user_passes_test(lambda u: u.is_superuser, login_url='/')  # в качестве параметра можем пердать login_url для переадресации на гл. страницу
+def index(request):
+    return render(request, 'adminapp/index.html')
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_users_read(request):
+    context = {'users': User.objects.all()}
+    return render(request, 'adminapp/admin-users-read.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users_read'))
+    else:
+        form = UserAdminRegisterForm()
+    context = {'form': form}
+    return render(request, 'adminapp/admin-users-create.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_users_update(request, id):
+    user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users_read'))
+    else:
+        form = UserAdminProfileForm(instance=user)
+    context = {'form': form, 'current_user': user}
+    return render(request, 'adminapp/admin-users-update-delete.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_users_delete(request, id):
+    user = User.objects.get(id=id)
+    user.is_active = False # user.delete()
+    user.save()
+    return HttpResponseRedirect(reverse('admins:admin_users_read'))
+
