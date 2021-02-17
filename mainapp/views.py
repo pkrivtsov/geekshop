1f3f3bf0645687be0fd5553f669107d5e23e246c
pkrivtsov <krivtsov@live.com>
2021-02-18 00:22:37 +0300

    Added pagination, filtering by category and CBV in adminapp

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,18 +1,40 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+# from django.core.paginator import EmptyPage, PageNotAnInteger
+
 from mainapp.models import Product, ProductCategory
 import datetime
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'mainapp/index.html')
 
-def products(request, id=None):
-    print(id)
-    context = {
-        'title': datetime.datetime.now(),
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
-    return render(request, 'mainapp/products.html', context)
 
+# def products(request, category_id=None):
+#     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#     context = {
+#         'title': datetime.datetime.now(),
+#         'categories': ProductCategory.objects.all(),
+#         'products': products,
+#     }
+#     return render(request, 'mainapp/products.html', context)
+
+def products(request, category_id=None, page=1):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id).order_by('-price')
+    else:
+        products = Product.objects.all().order_by('-price')
+    per_page = 2
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page)
+    context = {'title': datetime.datetime.now(), 'categories': ProductCategory.objects.all(),
+               'products': products_paginator}
+    return render(request, 'mainapp/products.html', context)
+    # try:
+    #     products_paginator = paginator.page(page)
+    # except PageNotAnInteger:
+    #     products_paginator = paginator.page(1)
+    # except EmptyPage:
+    #     products_paginator = paginator.page(paginator.num_pages)
